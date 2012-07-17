b1e206f3dd8594b5dcae7c75adc67e98e724d57b
Michael Schoonmaker <michael.schoonmaker@redrobotlabs.com>
2012-07-16 22:42:30 -0700

    Added timeline and new token to config.

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
     apiKey: "21ff1badf4787a18f528322514810e37",
     secret: "509a8d7b011eeafb",
-    token: 'eaa7266074c6bd282773e3dcf7bce0d9b426f32f'
+    token: '4b8ad1d2d84548d3e0a9adb2b79f2ce8867c258d',
+    timeline: '722298672'
 };
