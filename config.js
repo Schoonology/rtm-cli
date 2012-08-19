adc1de88662106b01ac4f50178cb9882a2c21af8
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2012-08-18 20:54:27 -0700

    Refactored a lot into the rtm Session.

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
     apiKey: "21ff1badf4787a18f528322514810e37",
     secret: "509a8d7b011eeafb",
-    token: '4b8ad1d2d84548d3e0a9adb2b79f2ce8867c258d',
     timeline: '722298672'
 };
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-var RTMClient = require('../rtm'),
+var rtm = require('../rtm'),
     repl = require('repl'),
     optimist = require('optimist'),
-    Cache = require('./lib/cache'),
-    stepup = require('stepup'),
-    open = require('open'),
+    nconf = require('nconf'),
     argv = optimist
         .usage('Usage: $0 [command]')
         .describe('help', 'Show this help message, then exit.')
@@ -16,89 +14,29 @@ if (argv.help) {
     process.exit();
 }
 
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
+// TODO: Find the right place to put the config file.
+nconf.argv().env().file(__dirname + '/config.json');
 
-var client = new RTMClient(argv),
-    command = argv._[0],
-    cache = new Cache(),
-    user = null;
-
-function basicErrorHandler(err) {
-    console.error(err.stack);
-}
+var session = new rtm.Session(argv),
+    command = argv._[0];
 
 var commands = {
     'repl': function startRepl() {
         var context = repl.start({}).context;
-        context.client = client;
-        context.cache = cache;
+        context.session = session;
+        context.nconf = nconf;
         context.callback = function callback(err, data) {
             this.err = err;
             this.data = data;
             console.log('Done.');
         };
     },
-    'list': function taskList() {
-        stepup(basicErrorHandler, function openCache() {
-            cache.open(this);
-        }, function getPreviousToken() {
-            cache.get('token', this);
-        }, function checkToken(token) {
-            if (token != null) {
-                client.checkToken(token, this);
-                return;
-            }
-
-            stepup(basicErrorHandler, function getAuthURL() {
-                client.getDesktopAuthURL('write', this);
-            }, function openAuthURL(url) {
-                console.log('Once you have granted this application access to Remember the Milk, press any key to continue.');
-                open(url);
-
-                // TODO: This is where I don't like the semantics of step(up) with "this" being a Function.
-                // "this" should be an Object, with (like .parallel) members for both callback and event
-                // handler continuation generators.
-                process.stdin.once('data', function (chunk) { this(); }.bind(this));
-            }, function getToken() {
-                client.getToken(this);
-            }, function checkToken(token) {
-                client.checkToken(token, this);
-            }.bind(this));
-        }, function cacheToken(response) {
-            user = response.user;
-            cache.put('token', response.token, this);
-        }, function welcome() {
-            console.log('Welcome, ' + user.fullname + '!');
-            this();
-        }, function getLists() {
-            client.getLists(this);
-        }, function printLists(lists) {
-            console.log('Available lists:');
-            lists.forEach(function (list) {
-                if (list.archived === '1' || list.deleted === '1') {
-                    return;
-                }
-
-                console.log('[' + list.id + '] ' + list.name);
-            });
-            this();
-        }, function done() {
-            console.log('Done.');
-            process.exit();
+    'login': function login() {
+        session.login(function (err, data) {
+            console.log('Login successful:', data);
         });
     }
 };
-
-// 1. Check the current token stored in Alfred.
-//  a. If valid, continue.
-//  b. If invalid, log in and store the token in Alfred.
-// 2. Perform the requested action.
-// 3. If the requested action requires a Timeline, check for one in Alfred.
-//  a. If one exists, continue.
-//  b. If one doesn't exist, create a Timeline and store in Alfred.
-// 4. If we ever fail, check the error result.
-//  a. If it's because of a bad timeline or a bad token, remove them from Alfred and start over.
 
 var commandFn = commands[argv._[0]];
 if (commandFn == null) {
